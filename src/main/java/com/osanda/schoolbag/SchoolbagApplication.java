c46156a2dfe0bfda82f8d987949cef7dc6cea5d3
Osanda Wedamulla <osandaw@rsmetrics.com>
2020-05-12 20:24:21 +0530

    initial commit

---
+++ src/main/java/com/osanda/schoolbag/SchoolbagApplication.java
@@ -0,0 +1,13 @@
+package com.osanda.schoolbag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolbagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolbagApplication.class, args);
+	}
+
+}
